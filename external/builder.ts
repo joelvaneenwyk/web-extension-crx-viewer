89c9d554f7f3299a76451d569198febcb24df1b1
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-12 13:37:46 -0800

    First pass of fixes to switch to typescript

--- external/builder.ts
+++ external/builder.ts
@@ -1,11 +1,9 @@
 /* jshint node:true */
 /* globals cp, ls, mkdir, test */
 
-'use strict';
-
-var fs = require('fs'),
-    path = require('path'),
-    vm = require('vm');
+import fs from 'fs'
+import path from 'path'
+import vm from 'vm'
 
 /**
  * A simple preprocessor that is based on the Firefox preprocessor
@@ -35,240 +33,240 @@ var fs = require('fs'),
  * ++i;
  * //#endif
  */
-function preprocess(inFilename, outFilename, defines) {
+export function preprocess(inFilename: string, outFilename: string, defines: string[]) {
   // TODO make this really read line by line.
-  var lines = fs.readFileSync(inFilename).toString().split('\n');
-  var totalLines = lines.length;
-  var out = '';
-  var i = 0;
+  const lines = fs.readFileSync(inFilename).toString().split('\n')
+  const totalLines = lines.length
+  let out = ''
+  let i = 0
   function readLine() {
     if (i < totalLines) {
-      return lines[i++];
+      return lines[i++]
     }
-    return null;
+    return null
   }
-  var writeLine = (typeof outFilename === 'function' ? outFilename :
-    function(line) {
-      out += line + '\n';
-    });
-  function evaluateCondition(code) {
-    if (!code || !code.trim()) {
-      throw new Error('No JavaScript expression given at ' + loc());
+  const writeLine = (typeof outFilename === 'function'
+    ? outFilename
+    : function (line: string) {
+      out += line + '\n'
+    })
+  function evaluateCondition(code: string) {
+    if (!code?.trim()) {
+      throw new Error('No JavaScript expression given at ' + loc())
     }
     try {
-      return vm.runInNewContext(code, defines, {displayErrors: false});
+      return vm.runInNewContext(code, defines, { displayErrors: false })
     } catch (e) {
       throw new Error('Could not evaluate "' + code + '" at ' + loc() + '\n' +
-                      e.name + ': ' + e.message);
+        e.name + ': ' + e.message)
     }
   }
-  function include(file) {
-    var realPath = fs.realpathSync(inFilename);
-    var dir = path.dirname(realPath);
+  function include(file: string) {
+    const realPath = fs.realpathSync(inFilename)
+    const dir = path.dirname(realPath)
     try {
-      var fullpath;
+      let fullpath
       if (file.indexOf('$ROOT/') === 0) {
         fullpath = path.join(__dirname, '../..',
-          file.substring('$ROOT/'.length));
+          file.substring('$ROOT/'.length))
       } else {
-        fullpath = path.join(dir, file);
+        fullpath = path.join(dir, file)
       }
-      preprocess(fullpath, writeLine, defines);
+      preprocess(fullpath, writeLine, defines)
     } catch (e) {
       if (e.code === 'ENOENT') {
-        throw new Error('Failed to include "' + file + '" at ' + loc());
+        throw new Error('Failed to include "' + file + '" at ' + loc())
       }
-      throw e; // Some other error
+      throw e // Some other error
     }
   }
-  function expand(line) {
-    line = line.replace(/__[\w]+__/g, function(variable) {
-      variable = variable.substring(2, variable.length - 2);
+  function expand(line: string) {
+    line = line.replace(/__[\w]+__/g, function (variable: string) {
+      variable = variable.substring(2, variable.length - 2)
       if (variable in defines) {
-        return defines[variable];
+        return defines[variable]
       }
-      return '';
-    });
-    writeLine(line);
+      return ''
+    })
+    writeLine(line)
   }
 
   // not inside if or else (process lines)
-  var STATE_NONE = 0;
+  const STATE_NONE = 0
   // inside if, condition false (ignore until #else or #endif)
-  var STATE_IF_FALSE = 1;
+  const STATE_IF_FALSE = 1
   // inside else, #if was false, so #else is true (process lines until #endif)
-  var STATE_ELSE_TRUE = 2;
+  const STATE_ELSE_TRUE = 2
   // inside if, condition true (process lines until #else or #endif)
-  var STATE_IF_TRUE = 3;
+  const STATE_IF_TRUE = 3
   // inside else or elif, #if/#elif was true, so following #else or #elif is
   // false (ignore lines until #endif)
-  var STATE_ELSE_FALSE = 4;
+  const STATE_ELSE_FALSE = 4
 
-  var line;
-  var state = STATE_NONE;
-  var stack = [];
-  var control =
-  /* jshint -W101 */
-    /^(?:\/\/|<!--)\s*#(if|elif|else|endif|expand|include|error)\b(?:\s+(.*?)(?:-->)?$)?/;
+  let line
+  let state = STATE_NONE
+  const stack: number[] = []
+  const control =
+    /* jshint -W101 */
+    /^(?:\/\/|<!--)\s*#(if|elif|else|endif|expand|include|error)\b(?:\s+(.*?)(?:-->)?$)?/
   /* jshint +W101 */
-  var lineNumber = 0;
-  var loc = function() {
-    return fs.realpathSync(inFilename) + ':' + lineNumber;
-  };
+  let lineNumber = 0
+  var loc = function () {
+    return fs.realpathSync(inFilename) + ':' + lineNumber
+  }
   while ((line = readLine()) !== null) {
-    ++lineNumber;
-    var m = control.exec(line);
+    ++lineNumber
+    const m = control.exec(line)
     if (m) {
       switch (m[1]) {
         case 'if':
-          stack.push(state);
-          state = evaluateCondition(m[2]) ? STATE_IF_TRUE : STATE_IF_FALSE;
-          break;
+          stack.push(state)
+          state = evaluateCondition(m[2]) ? STATE_IF_TRUE : STATE_IF_FALSE
+          break
         case 'elif':
           if (state === STATE_IF_TRUE || state === STATE_ELSE_FALSE) {
-            state = STATE_ELSE_FALSE;
+            state = STATE_ELSE_FALSE
           } else if (state === STATE_IF_FALSE) {
-            state = evaluateCondition(m[2]) ? STATE_IF_TRUE : STATE_IF_FALSE;
+            state = evaluateCondition(m[2]) ? STATE_IF_TRUE : STATE_IF_FALSE
           } else if (state === STATE_ELSE_TRUE) {
-            throw new Error('Found #elif after #else at ' + loc());
+            throw new Error('Found #elif after #else at ' + loc())
           } else {
-            throw new Error('Found #elif without matching #if at ' + loc());
+            throw new Error('Found #elif without matching #if at ' + loc())
           }
-          break;
+          break
         case 'else':
           if (state === STATE_IF_TRUE || state === STATE_ELSE_FALSE) {
-            state = STATE_ELSE_FALSE;
+            state = STATE_ELSE_FALSE
           } else if (state === STATE_IF_FALSE) {
-            state = STATE_ELSE_TRUE;
+            state = STATE_ELSE_TRUE
           } else {
-            throw new Error('Found #else without matching #if at ' + loc());
+            throw new Error('Found #else without matching #if at ' + loc())
           }
-          break;
+          break
         case 'endif':
           if (state === STATE_NONE) {
-            throw new Error('Found #endif without #if at ' + loc());
+            throw new Error('Found #endif without #if at ' + loc())
           }
-          state = stack.pop();
-          break;
+          state = stack.pop()
+          break
         case 'expand':
           if (state !== STATE_IF_FALSE && state !== STATE_ELSE_FALSE) {
-            expand(m[2]);
+            expand(m[2])
           }
-          break;
+          break
         case 'include':
           if (state !== STATE_IF_FALSE && state !== STATE_ELSE_FALSE) {
-            include(m[2]);
+            include(m[2])
           }
-          break;
+          break
         case 'error':
           if (state !== STATE_IF_FALSE && state !== STATE_ELSE_FALSE) {
-            throw new Error('Found #error ' + m[2] + ' at ' + loc());
+            throw new Error('Found #error ' + m[2] + ' at ' + loc())
           }
-          break;
+          break
       }
     } else {
       if (state === STATE_NONE) {
-        writeLine(line);
+        writeLine(line)
       } else if ((state === STATE_IF_TRUE || state === STATE_ELSE_TRUE) &&
-          stack.indexOf(STATE_IF_FALSE) === -1 &&
-          stack.indexOf(STATE_ELSE_FALSE) === -1) {
-        writeLine(line.replace(/^\/\/|^<!--|-->$/g, '  '));
+        !stack.includes(STATE_IF_FALSE) &&
+        !stack.includes(STATE_ELSE_FALSE)) {
+        writeLine(line.replace(/^\/\/|^<!--|-->$/g, '  '))
       }
     }
   }
   if (state !== STATE_NONE || stack.length !== 0) {
     throw new Error('Missing #endif in preprocessor for ' +
-                    fs.realpathSync(inFilename));
+      fs.realpathSync(inFilename))
   }
   if (typeof outFilename !== 'function') {
-    fs.writeFileSync(outFilename, out);
+    fs.writeFileSync(outFilename, out)
   }
 }
-exports.preprocess = preprocess;
 
-var deprecatedInMozcentral = new RegExp('(^|\\W)(' + [
-    '-moz-box-sizing',
-    '-moz-grab',
-    '-moz-grabbing'
-  ].join('|') + ')');
+const deprecatedInMozcentral = new RegExp('(^|\\W)(' + [
+  '-moz-box-sizing',
+  '-moz-grab',
+  '-moz-grabbing'
+].join('|') + ')')
 
-function preprocessCSS(mode, source, destination) {
+export function preprocessCSS(mode: string, source: string, destination: string) {
   function hasPrefixedFirefox(line) {
-    return (/(^|\W)-(ms|o|webkit)-\w/.test(line));
+    return (/(^|\W)-(ms|o|webkit)-\w/.test(line))
   }
 
   function hasPrefixedMozcentral(line) {
     return (/(^|\W)-(ms|o|webkit)-\w/.test(line) ||
-            deprecatedInMozcentral.test(line));
+      deprecatedInMozcentral.test(line))
   }
 
   function expandImports(content, baseUrl) {
     return content.replace(/^\s*@import\s+url\(([^\)]+)\);\s*$/gm,
-        function(all, url) {
-      var file = path.join(path.dirname(baseUrl), url);
-      var imported = fs.readFileSync(file, 'utf8').toString();
-      return expandImports(imported, file);
-    });
+      function (all, url) {
+        const file = path.join(path.dirname(baseUrl), url)
+        const imported = fs.readFileSync(file, 'utf8').toString()
+        return expandImports(imported, file)
+      })
   }
 
-  function removePrefixed(content, hasPrefixedFilter) {
-    var lines = content.split(/\r?\n/g);
-    var i = 0;
+  function removePrefixed(content: string, hasPrefixedFilter) {
+    const lines = content.split(/\r?\n/g)
+    let i = 0
     while (i < lines.length) {
-      var line = lines[i];
+      const line = lines[i]
       if (!hasPrefixedFilter(line)) {
-        i++;
-        continue;
+        i++
+        continue
       }
       if (/\{\s*$/.test(line)) {
-        var bracketLevel = 1;
-        var j = i + 1;
+        let bracketLevel = 1
+        let j = i + 1
         while (j < lines.length && bracketLevel > 0) {
-          var checkBracket = /([{}])\s*$/.exec(lines[j]);
+          const checkBracket = /([{}])\s*$/.exec(lines[j])
           if (checkBracket) {
             if (checkBracket[1] === '{') {
-              bracketLevel++;
+              bracketLevel++
             } else if (lines[j].indexOf('{') < 0) {
-              bracketLevel--;
+              bracketLevel--
             }
           }
-          j++;
+          j++
         }
-        lines.splice(i, j - i);
+        lines.splice(i, j - i)
       } else if (/[};]\s*$/.test(line)) {
-        lines.splice(i, 1);
+        lines.splice(i, 1)
       } else {
         // multiline? skipping until next directive or bracket
         do {
-          lines.splice(i, 1);
+          lines.splice(i, 1)
         } while (i < lines.length &&
-                 !/\}\s*$/.test(lines[i]) &&
-                 lines[i].indexOf(':') < 0);
+        !/\}\s*$/.test(lines[i]) &&
+          lines[i].indexOf(':') < 0)
         if (i < lines.length && /\S\s*}\s*$/.test(lines[i])) {
-          lines[i] = lines[i].substr(lines[i].indexOf('}'));
+          lines[i] = lines[i].substr(lines[i].indexOf('}'))
         }
       }
-      // collapse whitespaces
+      // collapse whitespace
       while (lines[i] === '' && lines[i - 1] === '') {
-        lines.splice(i, 1);
+        lines.splice(i, 1)
       }
     }
-    return lines.join('\n');
+    return lines.join('\n')
   }
 
   if (!mode) {
-    throw new Error('Invalid CSS preprocessor mode');
+    throw new Error('Invalid CSS preprocessor mode')
   }
 
-  var content = fs.readFileSync(source, 'utf8').toString();
-  content = expandImports(content, source);
+  let content = fs.readFileSync(source, 'utf8').toString()
+  content = expandImports(content, source)
   if (mode === 'mozcentral' || mode === 'firefox') {
-    content = removePrefixed(content, mode === 'mozcentral' ?
-                             hasPrefixedMozcentral : hasPrefixedFirefox);
+    content = removePrefixed(content, mode === 'mozcentral'
+      ? hasPrefixedMozcentral
+      : hasPrefixedFirefox)
   }
-  fs.writeFileSync(destination, content);
+  fs.writeFileSync(destination, content)
 }
-exports.preprocessCSS = preprocessCSS;
 
 /**
  * Simplifies common build steps.
@@ -279,55 +277,53 @@ exports.preprocessCSS = preprocessCSS;
  *        .preprocess array of arrays of source and destination pairs of files
  *                    run through preprocessor.
  */
-function build(setup) {
-  var defines = setup.defines;
+export function build(setup) {
+  const defines = setup.defines;
 
-  (setup.mkdirs || []).forEach(function(directory) {
-    mkdir('-p', directory);
-  });
+  (setup.mkdirs || []).forEach(function (directory) {
+    mkdir('-p', directory)
+  })
 
-  setup.copy.forEach(function(option) {
-    var source = option[0];
-    var destination = option[1];
-    cp('-R', source, destination);
-  });
+  setup.copy.forEach(function (option) {
+    const source = option[0]
+    const destination = option[1]
+    cp('-R', source, destination)
+  })
 
-  setup.preprocess.forEach(function(option) {
-    var sources = option[0];
-    var destination = option[1];
+  setup.preprocess.forEach(function (option) {
+    let sources = option[0]
+    const destination = option[1]
 
-    sources = ls('-R', sources);
-    sources.forEach(function(source) {
+    sources = ls('-R', sources)
+    sources.forEach(function (source) {
       // ??? Warn if the source is wildcard and dest is file?
-      var destWithFolder = destination;
+      let destWithFolder = destination
       if (test('-d', destination)) {
-        destWithFolder += '/' + path.basename(source);
+        destWithFolder += '/' + path.basename(source)
       }
-      preprocess(source, destWithFolder, defines);
-    });
+      preprocess(source, destWithFolder, defines)
+    })
   });
 
-  (setup.preprocessCSS || []).forEach(function(option) {
-    var mode = option[0];
-    var source = option[1];
-    var destination = option[2];
-    preprocessCSS(mode, source, destination);
-  });
+  (setup.preprocessCSS || []).forEach(function (option) {
+    const mode = option[0]
+    const source = option[1]
+    const destination = option[2]
+    preprocessCSS(mode, source, destination)
+  })
 }
-exports.build = build;
 
 /**
  * Merge two defines arrays. Values in the second param will override values in
  * the first.
  */
-function merge(defaults, defines) {
-  var ret = {};
+export function merge(defaults, defines) {
+  const ret = {}
   for (var key in defaults) {
-    ret[key] = defaults[key];
+    ret[key] = defaults[key]
   }
   for (key in defines) {
-    ret[key] = defines[key];
+    ret[key] = defines[key]
   }
-  return ret;
+  return ret
 }
-exports.merge = merge;
--- make.ts
+++ make.ts
@@ -7,12 +7,10 @@
 
 // Build tool, adapted from PDF.js
 /* jshint node:true */
-/* globals cd, cp, echo, exec, find, grep, mkdir, rm, target, test */
 
-'use strict';
-require('shelljs/make');
-var builder = require('./external/builder');
+import { exec, cd, echo, mkdir, rm, test, grep, find } from 'shelljs';
 
+import { build as builder } from './external/builder';
 
 var ROOT_DIR = __dirname + '/';
 var BUILD_DIR = ROOT_DIR + 'dist/';
@@ -22,201 +20,201 @@ var OPERA_BUILD_DIR = BUILD_DIR + 'opera/';
 var FIREFOX_BUILD_DIR = BUILD_DIR + 'firefox/';
 var WEB_BUILD_DIR = BUILD_DIR + 'web/';
 var ALLOWED_FILES = [
-    'manifest.json',
-    '.css',
-    '.html',
-    '.js',
-    '.png',
+  'manifest.json',
+  '.css',
+  '.html',
+  '.js',
+  '.png',
 ];
 
 function getVersionString() {
-    cd(ROOT_DIR);
-    var gitHash = exec('git log --format=%H -n1', {silent: true}).stdout.trim();
-    return gitHash || '(unknown version)';
+  cd(ROOT_DIR);
+  var gitHash = exec('git log --format=%H -n1', { silent: true }).stdout.trim();
+  return gitHash || '(unknown version)';
 }
 
-function getBuildConfig(options) {
-    var dest_dir = options.build_dir;
-    var setup = {
-        defines: {
-            CHROME: false,
-            FIREFOX: false,
-            OPERA: false,
-            WEB: false,
-            VERSION: getVersionString(),
-        },
-        mkdirs: [
-            dest_dir + 'icons',
-        ],
-        copy: [
-            [SRC_DIR + 'lib', dest_dir],
-            [SRC_DIR + 'icons/*.png', dest_dir + 'icons']
-        ],
-        preprocess: [
-            [SRC_DIR + '*.html', dest_dir],
-            [SRC_DIR + '*.js', dest_dir],
-            [SRC_DIR + 'lib/crx-to-zip.js', dest_dir + 'lib'],
-        ]
-    };
-    if (options.defines)
-        for (var key in options.defines)
-            setup.defines[key] = options.defines[key];
-    if (options.copy) [].push.apply(setup.copy, options.copy);
-    if (options.preprocess) [].push.apply(setup.preprocess, options.preprocess);
+function getBuildConfig(options: any) {
+  var dest_dir = options.build_dir;
+  var setup = {
+    defines: {
+      CHROME: false,
+      FIREFOX: false,
+      OPERA: false,
+      WEB: false,
+      VERSION: getVersionString(),
+    },
+    mkdirs: [
+      dest_dir + 'icons',
+    ],
+    copy: [
+      [SRC_DIR + 'lib', dest_dir],
+      [SRC_DIR + 'icons/*.png', dest_dir + 'icons']
+    ],
+    preprocess: [
+      [SRC_DIR + '*.html', dest_dir],
+      [SRC_DIR + '*.js', dest_dir],
+      [SRC_DIR + 'lib/crx-to-zip.js', dest_dir + 'lib'],
+    ]
+  };
+  if (options.defines)
+    for (var key in options.defines)
+      setup.defines[key] = options.defines[key];
+  if (options.copy) [].push.apply(setup.copy, options.copy);
+  if (options.preprocess) [].push.apply(setup.preprocess, options.preprocess);
 
-    return setup;
+  return setup;
 }
-function cleanDirectory(dir) {
-    if (test('-d', dir))
-        rm('-rf', dir + '*'); // Append wildcard to preserve the directory
-    else
-        mkdir('-p', dir);
+function cleanDirectory(dir: string) {
+  if (test('-d', dir))
+    rm('-rf', dir + '*'); // Append wildcard to preserve the directory
+  else
+    mkdir('-p', dir);
 }
 
-function build(setup, output_root_dir) {
-    cleanDirectory(output_root_dir);
-    builder.build(setup);
-    exec(ROOT_DIR + '/node_modules/.bin/lessc --strict-math=on "' + SRC_DIR + 'crxviewer.less" "' + output_root_dir + 'crxviewer.css"');
+function build(setup, output_root_dir: string) {
+  cleanDirectory(output_root_dir);
+  builder.build(setup);
+  exec(ROOT_DIR + '/node_modules/.bin/lessc --strict-math=on "' + SRC_DIR + 'crxviewer.less" "' + output_root_dir + 'crxviewer.css"');
 }
 
 function removeFirefoxSpecificFiles() {
-    rm('domain-fronter.js');
+  rm('domain-fronter.js');
 }
 function removeTestFiles() {
-    rm('lib/prettify/test-prism-language-detect.js');
-    rm('lib/prettify/test-prism-li-lines.js');
-    rm('lib/prettify/test-prism-source-extensions.html');
-    rm('test-asn1.html');
+  rm('lib/prettify/test-prism-language-detect.js');
+  rm('lib/prettify/test-prism-li-lines.js');
+  rm('lib/prettify/test-prism-source-extensions.html');
+  rm('test-asn1.html');
 }
 
-function lintDir(dest_dir) {
-    var warningCount = 0;
-    find(dest_dir).forEach(function(filepath) {
-        if (/\/\.[^\/]*$/.test(filepath)) {
-            echo('WARNING: Found dot file: ' + filepath);
-            ++warningCount;
-            return;
-        }
-        if (test('-d', filepath)) {
-            return;
-        }
-        var unprocessed = grep('//#', filepath).trim();
-        if (unprocessed) {
-            echo('WARNING: unprocessed text in' + filepath);
-            echo(unprocessed);
-            ++warningCount;
-        }
-        if (!ALLOWED_FILES.some(function(p) {
-            return p.test ? p.test(filepath) : filepath.endsWith(p);
-        })) {
-            echo('WARNING: Unrecognized file: ' + filepath);
-            ++warningCount;
-        }
-    });
-    if (warningCount) {
-        process.nextTick(function() {
-            echo('WARNING: ' + warningCount + ' warnings in ' + dest_dir);
-        });
+function lintDir(dest_dir: string) {
+  var warningCount = 0;
+  find(dest_dir).forEach(function (filepath: string) {
+    if (/\/\.[^\/]*$/.test(filepath)) {
+      echo('WARNING: Found dot file: ' + filepath);
+      ++warningCount;
+      return;
     }
+    if (test('-d', filepath)) {
+      return;
+    }
+    var unprocessed = grep('//#', filepath).trim();
+    if (unprocessed) {
+      echo('WARNING: unprocessed text in' + filepath);
+      echo(unprocessed);
+      ++warningCount;
+    }
+    if (!ALLOWED_FILES.some(function (p) {
+      return p.test ? p.test(filepath) : filepath.endsWith(p);
+    })) {
+      echo('WARNING: Unrecognized file: ' + filepath);
+      ++warningCount;
+    }
+  });
+  if (warningCount) {
+    process.nextTick(function () {
+      echo('WARNING: ' + warningCount + ' warnings in ' + dest_dir);
+    });
+  }
 }
 
-target.all = function() {
-    target.chrome();
-    target.opera();
-    target.firefox();
-    target.web();
+function target_chrome() {
+  echo();
+  echo('Building Chrome extension...');
+  var setup = getBuildConfig({
+    build_dir: CHROME_BUILD_DIR,
+    defines: {
+      CHROME: true
+    },
+    copy: [
+      [SRC_DIR + 'manifest.json', CHROME_BUILD_DIR]
+    ]
+  });
+  build(setup, CHROME_BUILD_DIR);
+
+  cd(CHROME_BUILD_DIR);
+  removeFirefoxSpecificFiles();
+  removeTestFiles();
+  rm('-f', '../crxviewer.zip');
+  exec('7z a ../crxviewer.zip * -tzip');
+  lintDir(CHROME_BUILD_DIR);
 };
 
-target.chrome = function() {
-    echo();
-    echo('Building Chrome extension...');
-    var setup = getBuildConfig({
-        build_dir: CHROME_BUILD_DIR,
-        defines: {
-            CHROME: true
-        },
-        copy: [
-            [SRC_DIR + 'manifest.json', CHROME_BUILD_DIR]
-        ]
-    });
-    build(setup, CHROME_BUILD_DIR);
+function target_opera() {
+  echo();
+  echo('Building Opera extension...');
+  var setup = getBuildConfig({
+    build_dir: OPERA_BUILD_DIR,
+    defines: {
+      OPERA: true
+    }
+  });
+  build(setup, OPERA_BUILD_DIR);
+  cp(SRC_DIR + 'manifest_opera.json', OPERA_BUILD_DIR + 'manifest.json');
 
-    cd(CHROME_BUILD_DIR);
-    removeFirefoxSpecificFiles();
-    removeTestFiles();
-    rm('-f', '../crxviewer.zip');
-    exec('7z a ../crxviewer.zip * -tzip');
-    lintDir(CHROME_BUILD_DIR);
+  cd(OPERA_BUILD_DIR);
+  removeFirefoxSpecificFiles();
+  removeTestFiles();
+  rm('-f', '../crxviewer_opera.zip');
+  exec('7z a ../crxviewer_opera.zip * -tzip');
+  lintDir(OPERA_BUILD_DIR);
 };
 
-target.opera = function() {
-    echo();
-    echo('Building Opera extension...');
-    var setup = getBuildConfig({
-        build_dir: OPERA_BUILD_DIR,
-        defines: {
-            OPERA: true
-        }
-    });
-    build(setup, OPERA_BUILD_DIR);
-    cp(SRC_DIR + 'manifest_opera.json', OPERA_BUILD_DIR + 'manifest.json');
+function target_firefox() {
+  echo();
+  echo('Building Firefox addon...');
+  var setup = getBuildConfig({
+    build_dir: FIREFOX_BUILD_DIR,
+    defines: {
+      FIREFOX: true
+    }
+  });
+  build(setup, FIREFOX_BUILD_DIR);
+  cp(SRC_DIR + 'manifest_firefox.json', FIREFOX_BUILD_DIR + 'manifest.json');
 
-    cd(OPERA_BUILD_DIR);
-    removeFirefoxSpecificFiles();
-    removeTestFiles();
-    rm('-f', '../crxviewer_opera.zip');
-    exec('7z a ../crxviewer_opera.zip * -tzip');
-    lintDir(OPERA_BUILD_DIR);
+  cd(FIREFOX_BUILD_DIR);
+  removeTestFiles();
+  // Firefox version uses event page instead of service worker.
+  rm('service_worker.js');
+  rm('-f', '../crxviewer_firefox.zip');
+  exec('7z a ../crxviewer_firefox.zip * -tzip');
+  lintDir(FIREFOX_BUILD_DIR);
 };
 
-target.firefox = function() {
-    echo();
-    echo('Building Firefox addon...');
-    var setup = getBuildConfig({
-        build_dir: FIREFOX_BUILD_DIR,
-        defines: {
-            FIREFOX: true
-        }
-    });
-    build(setup, FIREFOX_BUILD_DIR);
-    cp(SRC_DIR + 'manifest_firefox.json', FIREFOX_BUILD_DIR + 'manifest.json');
-
-    cd(FIREFOX_BUILD_DIR);
-    removeTestFiles();
-    // Firefox version uses event page instead of service worker.
-    rm('service_worker.js');
-    rm('-f', '../crxviewer_firefox.zip');
-    exec('7z a ../crxviewer_firefox.zip * -tzip');
-    lintDir(FIREFOX_BUILD_DIR);
+function target_web  {
+  echo();
+  echo('Building online demo...');
+  var dest_dir = WEB_BUILD_DIR;
+  var setup = {
+    defines: {
+      CHROME: false,
+      FIREFOX: false,
+      OPERA: false,
+      WEB: true,
+      VERSION: getVersionString(),
+    },
+    copy: [
+      [SRC_DIR + 'search-worker.js', dest_dir],
+      [SRC_DIR + 'search-tools.js', dest_dir],
+      [SRC_DIR + 'lib', dest_dir],
+      [SRC_DIR + 'chrome-platform-info.js', dest_dir],
+      [SRC_DIR + 'asn1lite.js', dest_dir],
+      [SRC_DIR + 'mozcose.js', dest_dir],
+    ],
+    preprocess: [
+      [SRC_DIR + 'crxviewer.html', dest_dir],
+      [SRC_DIR + 'crxviewer.js', dest_dir],
+      [SRC_DIR + 'cws_pattern.js', dest_dir],
+      [SRC_DIR + 'lib/crx-to-zip.js', dest_dir + 'lib'],
+    ]
+  };
+  build(setup, WEB_BUILD_DIR);
+  lintDir(WEB_BUILD_DIR);
 };
 
-target.web = function() {
-    echo();
-    echo('Building online demo...');
-    var dest_dir = WEB_BUILD_DIR;
-    var setup = {
-        defines: {
-            CHROME: false,
-            FIREFOX: false,
-            OPERA: false,
-            WEB: true,
-            VERSION: getVersionString(),
-        },
-        copy: [
-            [SRC_DIR + 'search-worker.js', dest_dir],
-            [SRC_DIR + 'search-tools.js', dest_dir],
-            [SRC_DIR + 'lib', dest_dir],
-            [SRC_DIR + 'chrome-platform-info.js', dest_dir],
-            [SRC_DIR + 'asn1lite.js', dest_dir],
-            [SRC_DIR + 'mozcose.js', dest_dir],
-        ],
-        preprocess: [
-            [SRC_DIR + 'crxviewer.html', dest_dir],
-            [SRC_DIR + 'crxviewer.js', dest_dir],
-            [SRC_DIR + 'cws_pattern.js', dest_dir],
-            [SRC_DIR + 'lib/crx-to-zip.js', dest_dir + 'lib'],
-        ]
-    };
-    build(setup, WEB_BUILD_DIR);
-    lintDir(WEB_BUILD_DIR);
+target.all = function () {
+  target_chrome();
+  target_opera();
+  target_firefox();
+  target_web();
 };
